cf26c6d5fbc240eeda287b2345171bc2cc4cc4f1
Mathew Goldsborough <mat@onlyonet.com>
2023-10-14 14:50:20 -1000

    adding buttons and volunteer popup

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,6 @@
+
+
+document.querySelector('.klaviyo-form-volunteer').addEventListener('click', function (){
+  window._klOnsite = window._klOnsite || [];
+  window._klOnsite.push(['openForm', 'SXqP9S']);
+});
