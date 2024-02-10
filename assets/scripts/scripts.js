3ee7a92f4ecad62d057ed285bcd36212809097ef
Mathew Goldsborough <mat@onlyonet.com>
2024-02-10 09:39:25 -1000

    migration to astro

--- assets/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-
-
-document.querySelector('.klaviyo-form-volunteer').addEventListener('click', function (){
-  window._klOnsite = window._klOnsite || [];
-  window._klOnsite.push(['openForm', 'SXqP9S']);
-});
